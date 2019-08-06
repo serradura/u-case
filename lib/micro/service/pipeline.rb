92be22556f2e1e335bfc54aa3af714cc399784ce
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-08-06 09:16:58 -0300

    Optimize Micro::Service::Pipeline

--- lib/micro/service/pipeline.rb
+++ lib/micro/service/pipeline.rb
@@ -2,39 +2,43 @@
 
 module Micro
   module Service
-    class Pipeline
-      INVALID_COLLECTION =
-        'argument must be a collection of `Micro::Service::Base` classes'.freeze
-
-      def self.[](*services)
-        new(services)
-      end
-
-      def initialize(services)
-        @services = validate!(services)
-      end
-
-      def call(arg={})
-        @services.reduce(initial_result(arg)) do |result, service|
-          break result if result.failure?
-          service.call(result.value)
+    module Pipeline
+      class Reducer
+        def initialize(services)
+          @services = services
         end
-      end
 
-      private
-
-        def validate!(services)
-          Array(services).tap do |collection|
-            if collection.any? { |klass| !(klass < ::Micro::Service::Base) }
-              raise ArgumentError, INVALID_COLLECTION
-            end
+        def call(arg={})
+          @services.reduce(initial_result(arg)) do |result, service|
+            break result if result.failure?
+            service.call(result.value)
           end
         end
 
-        def initial_result(arg)
-          return arg if arg.is_a?(Micro::Service::Result)
-          Micro::Service::Result::Success(value: arg)
-        end
+        private
+
+          def initial_result(arg)
+            return arg if arg.is_a?(Micro::Service::Result)
+            Micro::Service::Result::Success(value: arg)
+          end
+      end
+
+      private_constant :Reducer
+
+      INVALID_SERVICES =
+        'argument must be a collection of `Micro::Service::Base` classes'.freeze
+
+      def self.[](*args)
+        self.new(args)
+      end
+
+      def self.new(args)
+        services = Array(args)
+
+        raise ArgumentError, INVALID_SERVICES if services.any? { |klass| !(klass < ::Micro::Service::Base) }
+
+        Reducer.new(services)
+      end
     end
   end
 end
