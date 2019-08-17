0448fecf662e05e3707615c482e708a40cc41e75
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-08-17 13:35:10 -0300

    Add new example (rake calculator)

---
+++ examples/calculator/calc/operation.rb
@@ -0,0 +1,39 @@
+class Operation < Micro::Service::Base
+  attributes :a, :b
+
+  private def result_of(operation_result)
+    attributes(:a, :operator, :b).merge(result: operation_result)
+  end
+
+  class Add < Operation
+    attribute :operator, '+'
+
+    def call!
+      Success(result_of(a + b))
+    end
+  end
+
+  class Subtract < Operation
+    attribute :operator, '-'
+
+    def call!
+      Success(result_of(a - b))
+    end
+  end
+
+  class Multiply < Operation
+    attribute :operator, 'x'
+
+    def call!
+      Success(result_of(a * b))
+    end
+  end
+
+  class Divide < Operation
+    attribute :operator, '/'
+
+    def call!
+      Success(result_of(a / b))
+    end
+  end
+end
---
+++ examples/calculator/calc/transform_into_numbers.rb
@@ -0,0 +1,18 @@
+class TransformIntoNumbers < Micro::Service::Base
+  attributes :a, :b
+
+  def call!
+    number_a, number_b = number(a), number(b)
+
+    if number_a && number_b
+      Success(a: number(a), b: number(b))
+    else
+      Failure(:not_a_number) { 'The arguments must contain only numeric values' }
+    end
+  end
+
+  private def number(value)
+    return value.to_i if value =~ /\A[\-,=]?\d+\z/
+    return value.to_f if value =~ /\A[\-,=]?\d+\.\d+\z/
+  end
+end
