a6d0a8aa5d28d1f062484eaa0d5a17c4fb08b6fb
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-08-22 23:50:10 -0300

    Improve the default failure type definition for safe services

--- lib/micro/service/safe.rb
+++ lib/micro/service/safe.rb
@@ -3,15 +3,25 @@
 module Micro
   module Service
     class Safe < Service::Base
-      def call
-        begin
-          super
-        rescue => exception
-          raise exception if Error::ByWrongUsage.check(exception)
-
-          Failure(:exception) { exception }
-        end
+      def self.failure_type(arg, type)
+        return :exception if type == :error && arg.is_a?(Exception)
+        type
       end
+
+      def call
+        __call
+      rescue => exception
+        raise exception if Error::ByWrongUsage.check(exception)
+        Failure(exception)
+      end
+
+      private
+
+        def Failure(arg = :error)
+          value = block_given? ? yield : arg
+          type = self.class.failure_type(value, block_given? ? arg : :error)
+          __get_result__.__set__(false, value, type, self)
+        end
     end
   end
 end
