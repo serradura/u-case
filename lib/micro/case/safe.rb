9e271431da6094ddf1d9fb75315e9fef693e58b5
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-11-13 22:49:01 -0300

    Fix the definition of Micro::Case::Safe.Flow() method

--- lib/micro/case/safe.rb
+++ lib/micro/case/safe.rb
@@ -3,10 +3,6 @@
 module Micro
   class Case
     class Safe < ::Micro::Case
-      def self.Flow(args)
-        Flow::Reducer.build(Array(args))
-      end
-
       def call
         super
       rescue => exception
