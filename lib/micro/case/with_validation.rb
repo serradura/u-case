be16453a73d1330bc9b84f5c23dc03358ee66252
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-11-19 01:40:15 -0300

    Allow disabling the auto validation per use case class

--- lib/micro/case/with_validation.rb
+++ lib/micro/case/with_validation.rb
@@ -6,10 +6,24 @@ module Micro
   class Case
     include Micro::Attributes::Features::ActiveModelValidations
 
+    def self.auto_validation_disabled?
+      @disable_auto_validation
+    end
+
+    def self.disable_auto_validation
+      @disable_auto_validation = true
+    end
+
     def call
-      return Failure(:validation_error) { { errors: self.errors } } unless valid?
+      return failure_by_validation_error(self) if !self.class.auto_validation_disabled? && invalid?
 
       __call
+    end
+
+    private def failure_by_validation_error(object)
+      errors = object.respond_to?(:errors) ? object.errors : object
+
+      Failure(:validation_error) { { errors: errors } }
     end
   end
 end
