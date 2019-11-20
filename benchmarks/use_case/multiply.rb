086306b5e64c3807810f81b03b3df0b10bc4a892
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-11-20 00:38:02 -0300

    Update benchmarks (add dry-transaction and dry-monads)

---
+++ benchmarks/use_case/multiply.rb
@@ -0,0 +1,105 @@
+require 'dry/monads'
+require 'dry/monads/do'
+
+module Multiply
+  class WithInteractor
+    include Interactor
+
+    def call
+      a = context.a
+      b = context.b
+
+      if a.is_a?(Numeric) && b.is_a?(Numeric)
+        context.number = a * b
+      else
+        context.fail!(type: :invalid_data)
+      end
+    end
+  end
+
+  class WithMicroCase < Micro::Case
+    attributes :a, :b
+
+    def call!
+      if a.is_a?(Numeric) && b.is_a?(Numeric)
+        Success(a * b)
+      else
+        Failure(:invalid_data)
+      end
+    end
+  end
+
+  class WithMicroCaseStrict < Micro::Case::Strict
+    attributes :a, :b
+
+    def call!
+      if a.is_a?(Numeric) && b.is_a?(Numeric)
+        Success(a * b)
+      else
+        Failure(:invalid_data)
+      end
+    end
+  end
+
+  class WithMicroCaseSafe < Micro::Case::Safe
+    attributes :a, :b
+
+    def call!
+      if a.is_a?(Numeric) && b.is_a?(Numeric)
+        Success(a * b)
+      else
+        Failure(:invalid_data)
+      end
+    end
+  end
+
+  class WithDryTransaction
+    include Dry::Transaction
+
+    step :normalize
+    step :calculate
+
+    private
+
+    def normalize(input)
+      data = input.map { |key, value| [key.to_s, value] }.to_h
+
+      Success(data)
+    end
+
+    def calculate(input)
+      a = input['a']
+      b = input['b']
+
+      return Success(a * b) if a.is_a?(Numeric) && b.is_a?(Numeric)
+
+      Failure(:invalid_data)
+    end
+  end
+
+  class WithDryMonads
+    include Dry::Monads[:result]
+    include Dry::Monads::Do.for(:call)
+
+    def call(params)
+      input = yield normalize(params)
+      number = yield calculate(input['a'], input['b'])
+
+      Success(number)
+    end
+
+    private
+
+    def normalize(input)
+      data = input.map { |key, value| [key.to_s, value] }.to_h
+
+      Success(data)
+    end
+
+    def calculate(a, b)
+      return Success(a * b) if a.is_a?(Numeric) && b.is_a?(Numeric)
+
+      Failure(:invalid_data)
+    end
+  end
+end
