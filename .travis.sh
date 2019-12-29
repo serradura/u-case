bcfff2df172f84ec57b3aa322a382464169062d6
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-12-29 12:51:42 -0300

    Improve CI adding Ruby 2.7.0 and activemodel 6.0

--- .travis.sh
+++ .travis.sh
@@ -11,3 +11,8 @@ if [[ ! $ruby_v =~ '2.2.0' ]]; then
   ACTIVEMODEL_VERSION='5.2' bundle update
   ACTIVEMODEL_VERSION='5.2' bundle exec rake test
 fi
+
+if [[ $ruby_v =~ '2.5.' ]] || [[ $ruby_v =~ '2.6.' ]] || [[ $ruby_v =~ '2.7.' ]]; then
+  ACTIVEMODEL_VERSION='6.0' bundle update
+  ACTIVEMODEL_VERSION='6.0' bundle exec rake test
+fi
