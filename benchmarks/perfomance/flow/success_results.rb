8044f2997165cd7cf875faad7f464b081f08c150
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-10-14 14:17:17 -0300

    Update benchmarks/perfomance

--- benchmarks/perfomance/flow/success_results.rb
+++ benchmarks/perfomance/flow/success_results.rb
@@ -11,7 +11,7 @@ gemfile do
 
   gem 'interactor', '~> 3.1', '>= 3.1.1'
 
-  gem 'u-case', '~> 3.1.0'
+  gem 'u-case', '~> 4.1.0'
 end
 
 require_relative '../../examples/flow/add_five_with/all'
@@ -28,25 +28,25 @@ Benchmark.ips(&CallFlows.(
 ))
 
 # Warming up --------------------------------------
-# Interactor::Organizer            2.163k i/100ms
-# Micro::Cases.flow([])           13.158k i/100ms
-# Micro::Case flow in a class      8.400k i/100ms
-# Micro::Case including the class  8.008k i/100ms
-# Micro::Case::Result#|           17.151k i/100ms
-# Micro::Case::Result#then        14.121k i/100ms
+# Interactor::Organizer             1.809k i/100ms
+# Micro::Cases.flow([])             7.808k i/100ms
+# Micro::Case flow in a class       4.816k i/100ms
+# Micro::Case including the class   4.094k i/100ms
+# Micro::Case::Result#|             7.656k i/100ms
+# Micro::Case::Result#then          7.138k i/100ms
 
 # Calculating -------------------------------------
-# Interactor::Organizer            22.467k (± 1.8%) i/s -    112.476k in   5.007787s
-# Micro::Cases.flow([])           133.183k (± 1.5%) i/s -    671.058k in   5.039815s
-# Micro::Case flow in a class      84.083k (± 1.8%) i/s -    428.400k in   5.096623s
-# Micro::Case including the class  80.574k (± 1.6%) i/s -    408.408k in   5.070029s
-# Micro::Case::Result#|           172.734k (± 1.1%) i/s -    874.701k in   5.064429s
-# Micro::Case::Result#then        139.799k (± 1.7%) i/s -    706.050k in   5.052035s
+# Interactor::Organizer             24.290k (±24.0%) i/s -    113.967k in   5.032825s
+# Micro::Cases.flow([])             74.790k (±11.1%) i/s -    374.784k in   5.071740s
+# Micro::Case flow in a class       47.043k (± 8.0%) i/s -    235.984k in   5.047477s
+# Micro::Case including the class   42.030k (± 8.5%) i/s -    208.794k in   5.002138s
+# Micro::Case::Result#|             80.936k (±15.9%) i/s -    398.112k in   5.052531s
+# Micro::Case::Result#then          71.459k (± 8.8%) i/s -    356.900k in   5.030526s
 
 # Comparison:
-# Micro::Case::Result#|:          172734.4 i/s
-# Micro::Case::Result#then:       139799.0 i/s - 1.24x  (± 0.00) slower
-# Micro::Cases.flow([]):          133182.9 i/s - 1.30x  (± 0.00) slower
-# Micro::Case flow in a class:     84082.6 i/s - 2.05x  (± 0.00) slower
-# Micro::Case including the class: 80574.3 i/s - 2.14x  (± 0.00) slower
-# Interactor::Organizer:           22467.4 i/s - 7.69x  (± 0.00) slower
+# Micro::Case::Result#|:            80936.2 i/s
+# Micro::Cases.flow([]):            74790.1 i/s - same-ish: difference falls within error
+# Micro::Case::Result#then:         71459.5 i/s - same-ish: difference falls within error
+# Micro::Case flow in a class:      47042.6 i/s - 1.72x  (± 0.00) slower
+# Micro::Case including the class:  42030.2 i/s - 1.93x  (± 0.00) slower
+# Interactor::Organizer:            24290.3 i/s - 3.33x  (± 0.00) slower
