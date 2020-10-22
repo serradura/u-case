f8b2d6f0fb171524b1fd600fbd31828093b40aac
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-10-21 22:49:24 -0300

    Improve .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -3,36 +3,41 @@
 ruby_v=$(ruby -v)
 
 ACTIVERECORD_VERSION='3.2' bundle update
-ACTIVERECORD_VERSION='3.2' ENABLE_TRANSITIONS='false' bundle exec rake test
 ACTIVERECORD_VERSION='3.2' ENABLE_TRANSITIONS='true' bundle exec rake test
+ACTIVERECORD_VERSION='3.2' ENABLE_TRANSITIONS='false' bundle exec rake test
 
 ACTIVERECORD_VERSION='4.0' bundle update
-ACTIVERECORD_VERSION='4.0' bundle exec rake test
+ACTIVERECORD_VERSION='4.0' ENABLE_TRANSITIONS='true' bundle exec rake test
+ACTIVERECORD_VERSION='4.0' ENABLE_TRANSITIONS='false' bundle exec rake test
 
 ACTIVERECORD_VERSION='4.1' bundle update
-ACTIVERECORD_VERSION='4.1' bundle exec rake test
+ACTIVERECORD_VERSION='4.1' ENABLE_TRANSITIONS='true' bundle exec rake test
+ACTIVERECORD_VERSION='4.1' ENABLE_TRANSITIONS='false' bundle exec rake test
 
 ACTIVERECORD_VERSION='4.2' bundle update
-ACTIVERECORD_VERSION='4.2' bundle exec rake test
+ACTIVERECORD_VERSION='4.2' ENABLE_TRANSITIONS='true' bundle exec rake test
+ACTIVERECORD_VERSION='4.2' ENABLE_TRANSITIONS='false' bundle exec rake test
 
 ACTIVERECORD_VERSION='5.0' bundle update
-ACTIVERECORD_VERSION='5.0' bundle exec rake test
+ACTIVERECORD_VERSION='5.0' ENABLE_TRANSITIONS='true' bundle exec rake test
+ACTIVERECORD_VERSION='5.0' ENABLE_TRANSITIONS='false' bundle exec rake test
 
 ACTIVERECORD_VERSION='5.1' bundle update
-ACTIVERECORD_VERSION='5.1' bundle exec rake test
+ACTIVERECORD_VERSION='5.1' ENABLE_TRANSITIONS='true' bundle exec rake test
+ACTIVERECORD_VERSION='5.1' ENABLE_TRANSITIONS='false' bundle exec rake test
 
 if [[ ! $ruby_v =~ '2.2.0' ]]; then
   ACTIVERECORD_VERSION='5.2' bundle update
-  ACTIVERECORD_VERSION='5.2' ENABLE_TRANSITIONS='false' bundle exec rake test
   ACTIVERECORD_VERSION='5.2' ENABLE_TRANSITIONS='true' bundle exec rake test
+  ACTIVERECORD_VERSION='5.2' ENABLE_TRANSITIONS='false' bundle exec rake test
 fi
 
 if [[ $ruby_v =~ '2.5.' ]] || [[ $ruby_v =~ '2.6.' ]] || [[ $ruby_v =~ '2.7.' ]]; then
   ACTIVERECORD_VERSION='6.0' bundle update
-  ACTIVERECORD_VERSION='6.0' ENABLE_TRANSITIONS='false' bundle exec rake test
   ACTIVERECORD_VERSION='6.0' ENABLE_TRANSITIONS='true' bundle exec rake test
+  ACTIVERECORD_VERSION='6.0' ENABLE_TRANSITIONS='false' bundle exec rake test
 fi
 
 bundle update
-ENABLE_TRANSITIONS='false' bundle exec rake test
 ENABLE_TRANSITIONS='true' bundle exec rake test
+ENABLE_TRANSITIONS='false' bundle exec rake test
