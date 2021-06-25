fb40c5fc7b1486292be9c107a29b6f95be26a491
Tomás Coelho <tomascoelho6@gmail.com>
2021-06-25 09:44:39 -0300

    Add #keys, #fetch and #fetch_values to Micro::Case::Result

--- lib/micro/case/utils.rb
+++ lib/micro/case/utils.rb
@@ -3,11 +3,13 @@
 module Micro::Case::Utils
 
   module Hashes
-    def self.respond_to?(hash, method)
+    extend self
+
+    def respond_to?(hash, method)
       Kind::Hash[hash].respond_to?(method)
     end
 
-    def self.symbolize_keys(hash)
+    def symbolize_keys(hash)
       return hash.transform_keys { |key| key.to_sym rescue key } if respond_to?(hash, :transform_keys)
 
       hash.each_with_object({}) do |(k, v), memo|
@@ -16,16 +18,28 @@ module Micro::Case::Utils
       end
     end
 
-    def self.stringify_keys(hash)
+    def stringify_keys(hash)
       return hash.transform_keys(&:to_s) if respond_to?(hash, :transform_keys)
 
       hash.each_with_object({}) { |(k, v), memo| memo[k.to_s] = v }
     end
 
-    def self.slice(hash, keys)
+    def slice(hash, keys)
       return hash.slice(*keys) if respond_to?(hash, :slice)
 
       hash.select { |key, _value| keys.include?(key) }
+    end
+
+    def fetch_values(hash, keys, &block)
+      return hash.fetch_values(*keys, &block) if respond_to?(hash, :fetch_values)
+
+      result = []
+
+      keys.each do |key|
+        result << hash.fetch(key, &block)
+      end
+
+      result
     end
   end
 
