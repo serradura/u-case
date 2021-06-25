60dea5ba374efdd6bf242e0013afc125a8458f16
Tomás Coêlho <36938811+tomascco@users.noreply.github.com>
2021-06-25 16:37:22 -0300

    Apply suggestions from code review
    Improve `fetch_values` implementation
    
    Co-authored-by: Rodrigo Serradura <serradura@users.noreply.github.com>

--- lib/micro/case/utils.rb
+++ lib/micro/case/utils.rb
@@ -31,13 +31,9 @@ module Micro::Case::Utils
     def self.fetch_values(hash, keys, &block)
       return hash.fetch_values(*keys, &block) if hash_respond_to?(hash, :fetch_values)
 
-      result = []
-
-      keys.each do |key|
-        result << hash.fetch(key, &block)
+      keys.each_with_object([]) do |key, values|
+        values << hash.fetch(key, &block)
       end
-
-      result
     end
   end
 
