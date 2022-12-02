46c96f04513947828f3d6e90a335f3c7c6438a17
Bruno <bruno.larouche@gmail.com>
2022-12-01 19:49:26 -0500

    respond_to? renamed into utils.rb to avoid overriding default one

--- lib/micro/case/utils.rb
+++ lib/micro/case/utils.rb
@@ -3,12 +3,12 @@
 module Micro::Case::Utils
 
   module Hashes
-    def self.respond_to?(hash, method)
+    def self.hash_respond_to?(hash, method)
       Kind::Hash[hash].respond_to?(method)
     end
 
     def self.symbolize_keys(hash)
-      return hash.transform_keys { |key| key.to_sym rescue key } if respond_to?(hash, :transform_keys)
+      return hash.transform_keys { |key| key.to_sym rescue key } if hash_respond_to?(hash, :transform_keys)
 
       hash.each_with_object({}) do |(k, v), memo|
         key = k.to_sym rescue k
@@ -17,13 +17,13 @@ module Micro::Case::Utils
     end
 
     def self.stringify_keys(hash)
-      return hash.transform_keys(&:to_s) if respond_to?(hash, :transform_keys)
+      return hash.transform_keys(&:to_s) if hash_respond_to?(hash, :transform_keys)
 
       hash.each_with_object({}) { |(k, v), memo| memo[k.to_s] = v }
     end
 
     def self.slice(hash, keys)
-      return hash.slice(*keys) if respond_to?(hash, :slice)
+      return hash.slice(*keys) if hash_respond_to?(hash, :slice)
 
       hash.select { |key, _value| keys.include?(key) }
     end
